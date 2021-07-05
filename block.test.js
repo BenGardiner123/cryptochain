6b6453a3a5d55b1f017f71f3c559b713c7f6ecf3
Ben Gardiner <bennygards@gmail.com>
2021-07-05 13:33:01 +1000

    initial commit

---
+++ block.test.js
@@ -0,0 +1,18 @@
+const Block = require("./block");
+
+describe('Block', () => {
+    const timestamp = 'a-date';
+    const lastHash = 'foo-hash';
+    const hash = 'bar-hash';
+    const data = ['blockchain', 'data'];
+    //shortcut way to write this below inline without the this.timestamp = timestamp
+    const block = new Block({ timestamp, lastHash, hash, data});
+
+    it('has a timestamp, lastHash, hash and data property', () => {
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+    });
+});
+
