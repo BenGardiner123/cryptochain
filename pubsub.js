7d04f36fa52b00286a8bb1cce055c25d1f752d6a
Ben Gardiner <bennygards@gmail.com>
2021-07-07 11:42:46 +1000

    adding logic to handle - broadcasting the new chain and - updating the chain by getting the parsed message from the blockchain channel and replcaing the existing blockchain.

--- pubsub.js
+++ pubsub.js
@@ -7,11 +7,14 @@ const credentials = {
 }
 
 const CHANNELS = {
-    TEST: 'TEST'
+    TEST: 'TEST',
+    BLOCKCHAIN: "BLOCKCHAIN"
 };
 
 class PubSub {
-    constructor() {
+    constructor({ blockchain }) {
+        //makes the local instance of the blockchain = the incoming blockchain 
+        this.blockchain = blockchain;
         this.pubnub = new PubNub(credentials);
 
         //auto mapping the channels as they get added to the CHANNELS map above
@@ -27,8 +30,15 @@ class PubSub {
             message: messageObject => {
                 //TODO really need to research this destructuring in Javascript
                 const { channel, message } = messageObject;
+                
 
                 console.log(`Message recieved. Channel: ${channel}. Message: ${message}. `);
+
+                const parsedMessage = JSON.stringify(message);
+                if(channel == CHANNELS.BLOCKCHAIN){
+                    //the logic to confirm the chain is valid is inside replaceChain already
+                    this.blockchain.replaceChain(parsedMessage);
+                }
             }
         }
     }
@@ -37,9 +47,22 @@ class PubSub {
     publish({ channel, message }) {
         this.pubnub.publish({ channel, message });
     }
+
+    boradcastChain(){
+        this.publish({
+            channel: CHANNELS.BLOCKCHAIN,
+            //we can only publish string messages across the channels and blockchain.chain is an array - so JSON strinify it
+            message: JSON.stringify(this.blockchain.chain)
+        });
+    }
+
+
 }
 
-const testPubSub = new PubSub();
-testPubSub.publish({ channel: CHANNELS.TEST, message: "This is a first test" });
+// -------------------      test data --------------------------------------------
+// const testPubSub = new PubSub();
+// testPubSub.publish({ channel: CHANNELS.TEST, message: "This is a first test" });
+// //to test this i used *** node pubsub.js *** in the console
+
 
 module.exports = PubSub;
