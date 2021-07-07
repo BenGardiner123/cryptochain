e190cfaec3c52b9b783a07c842c710fef8da2b3e
Ben Gardiner <bennygards@gmail.com>
2021-07-07 11:28:06 +1000

    adding dummy test data

--- pubsub.js
+++ pubsub.js
@@ -13,7 +13,33 @@ const CHANNELS = {
 class PubSub {
     constructor() {
         this.pubnub = new PubNub(credentials);
+
         //auto mapping the channels as they get added to the CHANNELS map above
-        this.pubnub.subscribe({ channels: [Object.values(CHANNELS)] });
+        this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+
+        //using the built in pubnub add listener method 
+        this.pubnub.addListener(this.listener());
+    }
+
+    //listen out for messages then report the message to the console
+    listener() {
+        return {
+            message: messageObject => {
+                //TODO really need to research this destructuring in Javascript
+                const { channel, message } = messageObject;
+
+                console.log(`Message recieved. Channel: ${channel}. Message: ${message}. `);
+            }
+        }
+    }
+
+    //publish the message across the selected channel
+    publish({ channel, message }) {
+        this.pubnub.publish({ channel, message });
     }
 }
+
+const testPubSub = new PubSub();
+testPubSub.publish({ channel: CHANNELS.TEST, message: "This is a first test" });
+
+module.exports = PubSub;
