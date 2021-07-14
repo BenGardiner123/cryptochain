60cdf7f11ec14cc017f820525bf077466a86f218
Ben Gardiner <bennygards@gmail.com>
2021-07-14 10:31:41 +1000

    ordering the folders

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
-const PubSub = require('./pubsub');
+const PubSub = require('./app/pubsub');
 const request = require('request');
 const { response } = require('express');
 
